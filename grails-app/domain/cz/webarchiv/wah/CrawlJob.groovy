07eda5c2018b90a4633ee0ee9c56a606b9b811bc
Adam Brokes <adam.brokes@gmail.com>
2010-11-13 00:02:27 +0100

    Added CrawlStatus - based on Heritrix CrawlJob.getJobStatusDescription()

--- grails-app/domain/cz/webarchiv/wah/CrawlJob.groovy
+++ grails-app/domain/cz/webarchiv/wah/CrawlJob.groovy
@@ -1,16 +1,51 @@
 package cz.webarchiv.wah
 
 class CrawlJob {
-    Integer status
+    String name
+    CrawlStatus status
+    String statusDescription
     Integer priority
     String orderXml
 
+    Date dateCreated
+    Date lastUpdated
+
+    public CrawlJob(String name) {
+        this.name = name
+    }
+
     static constraints = {
+        name(blank: false, nullable: false)
+        priority(range: 1..3)
     }
 
     static mappings = {
         orderXml type: 'text'
     }
 
-    static hasMany = Seedlist
+    static hasMany = [seedlists: Seedlist]
+
+    static List<CrawlJob> getRunning() {
+        return CrawlJob.findAllByStatus(CrawlStatus.ACTIVE)
+    }
+
+}
+
+enum CrawlStatus {
+    UNBUILD('Unbuild'),
+    ACTIVE('Active'),
+    READY('Ready'),
+    FINISHED('Finished'),
+    UNKNOWN('Unknown')
+
+    String status
+
+    CrawlStatus(String status) {
+        this.status = status
+    }
+
+    static list() {
+        [UNBUILD, ACTIVE, READY, FINISHED, UNKNOWN]
+    }
+
 }
